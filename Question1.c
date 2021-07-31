d827b19be12803f4d75464611a65455238cea57e
Andrew Liutiev <a.liutiev@gmail.com>
2021-07-30 22:05:12 -0400

    added readme.md

---
+++ Question1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Github ID: Spartinii
+// Github Repo: https://github.com/Spartinii/cp386a4
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Input file name missing...exiting with error code -1\n");
+		return -1;
+	}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# cp386
+# cp386 - Bankers Algorithm
+
+# Motivation
+
+# Installation
+
+# Screenshots
+
+# Individual Contribution (Function wise)
+
+# Features
+
+# Test Cases
+
+# Use Examples
+
+# About Developers
+
+# Credits
+
+# Licenses to use Code
